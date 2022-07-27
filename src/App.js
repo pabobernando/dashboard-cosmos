d6a60e9c69d0ba863bd164f9cd997384e68bea19
Pabo Bernando <pabobernando69@gmail.com>
2022-07-28 02:43:31 +0700

    swap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css' ;
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState} from 'react';
+import { Button } from 'react-bootstrap';
+import { FaBeer } from 'react-icons/fa';
+import { FaSort } from 'react-icons/fa';
 
 function App() {
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container-fluid bg-dark'>
+        <div className='row'>
+          <div className='col-6'>
+          <h3 className='text-white'><FaBeer /> Kewr</h3>
+          </div>
+          <div className='col-6 text-end'>
+          <Button>Connect Keplr</Button>
+          </div>
+        </div>
+        <div className='row text-white text-center mt-5'>
+          <div className='col-4'>
+            <h4 className='border border-white'>Osmosis</h4>
+            <h5 className='mt-5'>Balance A</h5>
+            <h5>Balance B</h5>
+            <h5>Balance C</h5>
+          </div>
+          <div className='col-4'>
+            <h4 className='border border-white'>Iris</h4>
+            <h5 className='mt-5'>Balance A</h5>
+            <h5>Balance B</h5>
+            <h5>Balance C</h5>
+          </div>
+          <div className='col-4'>
+            <h4 className='border border-white'>Crescent</h4>
+            <h5 className='mt-5'>Balance A</h5>
+            <h5>Balance B</h5>
+            <h5>Balance C</h5>
+          </div>
+        </div>
+        <div className='row text-white text-center mt-5 border border-primary'>
+          <div className='col-4 border border-primary'>
+            <h4 className=''>Osmosis</h4>
+            <select className='mt-5'>
+              <option>Iris</option>
+              <option>Osmosis</option>
+              <option>Gravity</option>
+            </select>
+            <h4 className='mt-3'><FaSort/></h4>
+            <select className='mt-3'>
+              <option>Iris</option>
+              <option>Osmosis</option>
+              <option>Gravity</option>
+            </select>
+            <h4></h4>
+            <Button>Swap</Button>
+          </div>
+          <div className='col-4 border border-primary'>
+          <h4>Iris</h4>
+          <select className='mt-5'>
+              <option>Iris</option>
+              <option>Osmosis</option>
+              <option>Gravity</option>
+            </select>
+            <h4 className='mt-3'><FaSort/></h4>
+            <select className='mt-3'>
+              <option>Iris</option>
+              <option>Osmosis</option>
+              <option>Gravity</option>
+            </select>
+            <h4></h4>
+            <Button>Swap</Button>
+          </div>
+          <div className='col-4 border border-primary'>
+          <h4>Crescent</h4>
+          <select className='mt-5'>
+              <option>Iris</option>
+              <option>Osmosis</option>
+              <option>Gravity</option>
+            </select>
+            <h4 className='mt-3'><FaSort/></h4>
+            <select className='mt-3'>
+              <option>Iris</option>
+              <option>Osmosis</option>
+              <option>Gravity</option>
+            </select>
+            <h4></h4>
+            <Button>Swap</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
